1abfcac999650292d7ad70a906fdb8c296aa3ece
ludm <18660369596@163.com>
2018-07-31 16:40:53 +0800

    Spring Web MVC 框架

---
+++ trunk/SpringWebMVC/src/main/java/spring/services/log4j/HelloWorld.java
@@ -0,0 +1,10 @@
+package spring.services.log4j;
+public class HelloWorld {
+   private String message;
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+}
