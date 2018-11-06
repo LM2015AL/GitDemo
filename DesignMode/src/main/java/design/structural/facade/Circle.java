4e825b477908770daf377581fb3317a97f6650ca
Administrator <Administrator@USER-20181014FC>
2018-11-06 16:51:16 +0800

    外观模式

---
+++ DesignMode/src/main/java/design/structural/facade/Circle.java
@@ -0,0 +1,10 @@
+package design.structural.facade;
+
+public class Circle implements Shape {
+
+	public void draw() {
+
+		System.out.println("Circle");
+	}
+
+}
---
+++ DesignMode/src/main/java/design/structural/facade/Rectangle.java
@@ -0,0 +1,10 @@
+package design.structural.facade;
+
+public class Rectangle implements Shape {
+
+	public void draw() {
+
+		System.out.println("Rectangle");
+	}
+
+}
